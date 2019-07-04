1f9b386e24f1509d8e16404539c39c047b51566b
Gabriel Grijalva Flores <ardaga.trading@gmail.com>
2019-07-04 11:46:50 -0600

    Changed the modules export implementation.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-exports.BinanceRest = require('./lib/src/binance');
-exports.BitflyerRest = require('./lib/src/bitflyer');
-exports.BitmexWs = require('./lib/src/bitmex/ws');
-exports.BitmexRest = require('./lib/src/bitmex/rest');
-exports.HuobiDMWs = require('./lib/src/huobi-dm/ws');
-exports.HuobiDMRest = require('./lib/src/huobi-dm/rest');
+exports.BinanceRest = require('./lib/src/binance/binance');
+exports.BitflyerRest = require('./lib/src/bitflyer/bitflyer');
+exports.BitmexWs = require('./lib/src/bitmex/ws/bitmex-ws');
+exports.BitmexRest = require('./lib/src/bitmex/rest/bitmex-rest');
+exports.HuobiDMWs = require('./lib/src/huobi-dm/ws/huobi-dm-ws');
+exports.HuobiDMRest = require('./lib/src/huobi-dm/rest/huobi-dm-rest');
