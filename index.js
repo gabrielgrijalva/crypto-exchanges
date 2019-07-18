e63cab927d5f8b5a946cdd6304d8576270cc96d1
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2019-07-18 17:46:45 -0600

    Reorganized the bitflyer REST API module structure.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 exports.BinanceRest = require('./lib/src/binance/binance');
-exports.BitflyerRest = require('./lib/src/bitflyer/bitflyer');
+exports.BitflyerRest = require('./lib/src/bitflyer/rest/bitflyer-rest');
 exports.BitmexWs = require('./lib/src/bitmex/ws/bitmex-ws');
 exports.BitmexRest = require('./lib/src/bitmex/rest/bitmex-rest');
 exports.HuobiDMWs = require('./lib/src/huobi-dm/ws/huobi-dm-ws');
