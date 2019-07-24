a0254d1e44951793a30e831282e068fdca941df8
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2019-07-23 18:11:16 -0600

    Added BITFLYER Websocket API interface to main export module.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.BinanceRest = require('./lib/src/binance/binance');
+exports.BitflyerWs = require('./lib/src/bitflyer/ws/bitflyer-ws');
 exports.BitflyerRest = require('./lib/src/bitflyer/rest/bitflyer-rest');
 exports.BitmexWs = require('./lib/src/bitmex/ws/bitmex-ws');
 exports.BitmexRest = require('./lib/src/bitmex/rest/bitmex-rest');
