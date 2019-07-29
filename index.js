76c75c3323ddd25cb7db00f97cbcff9207dfed60
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2019-07-29 11:10:32 -0600

    Implemented 1forge REST API module.

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ exports.BitmexWs = require('./lib/src/bitmex/ws/bitmex-ws');
 exports.BitmexRest = require('./lib/src/bitmex/rest/bitmex-rest');
 exports.HuobiDMWs = require('./lib/src/huobi-dm/ws/huobi-dm-ws');
 exports.HuobiDMRest = require('./lib/src/huobi-dm/rest/huobi-dm-rest');
+exports.OneForge = require('./lib/src/one-forge/rest/one-forge-rest');
